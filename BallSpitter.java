acd423913832cfdb60663f30ad2707da14241849
SethPlattner <90936389+SethPlattner@users.noreply.github.com>
2021-12-03 15:41:15 -0800

    Add files via upload
    is a backup of the code on the laptop so that there is any history of it because the code was initially on ROOSEVELTS GITHUB AND WE HAVE NO IDEA WHY AND IT IS REALLY REALLY REALLY STRESSFUL but regardless here it is so here it is stable

---
+++ BallSpitter.java
@@ -0,0 +1,32 @@
+package frc.robot;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.SpeedController;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+public class  BallSpitter {
+
+    private final VictorSPX spitterWheel;
+    private final double speed = 0.5;
+
+    BallSpitter(final int spitterPort) {
+        spitterWheel = new VictorSPX(5);
+        
+  
+    }
+
+    
+
+
+
+    public void neutral() {
+        spitterWheel.set(ControlMode.PercentOutput, 0);
+    }
+
+    public void updateSpeed(final LambdaJoystick.ThrottlePosition throttlePosition){
+        spitterWheel.set(ControlMode.PercentOutput, throttlePosition.y);
+    }
+}
